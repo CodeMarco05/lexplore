f9e7a09aef36feec1137cc7a551bb839d3b03a61
Marco Brandt <marcobrandt@MBP-von-Marco.fritz.box>
2023-12-21 20:38:37 +0100

    pictures are loading with text

--- pages/collection/script.js
+++ pages/collection/script.js
@@ -1,10 +1,51 @@
 window.onload = function () {
     let queryParameterValue = getQueryParameter('query');
     if (queryParameterValue) {
-        document.getElementById('queryParameter').textContent = 'Query Parameter: ' + queryParameterValue;
+
+        let url = `https://images-api.nasa.gov/search?keywords=${queryParameterValue}&media_type=image&page_size=500`;
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                const pictureContainer = document.getElementById("pictureContainer")
+                // Handle the JSON response
+                let items = data.collection.items
+
+
+                console.log('API response:', items);
+
+                items.forEach(entry => {
+                    let link = (entry.links[0].href)
+
+                    const container = document.createElement("div")
+                    container.className = "picture-container"
+
+                    const imgElement = document.createElement("img")
+                    imgElement.src = link
+                    imgElement.width = 600
+
+
+                    const textElement = document.createElement('div');
+                    textElement.className = 'text-container';
+                    textElement.textContent = entry.data[0].description;
+
+
+                    container.appendChild(imgElement)
+                    container.appendChild(textElement)
+                    pictureContainer.appendChild(container)
+
+                });
+
+            })
+            .catch(error => {
+                console.error('Error during API call:', error);
+            });
     }
-    let url = `https://images-api.nasa.gov/search?keywords=${queryParameterValue}&media_type=image`;
-    console.log(url)
 }
 
 function getQueryParameter(name) {
